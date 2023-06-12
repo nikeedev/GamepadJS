e28b69642ed1876405a6ed12784ef36740b1b829
nikeedev <nikita.gon.dev@gmail.com>
2023-06-12 21:12:16 +0200

    Updates

--- script.js
+++ script.js
@@ -1,71 +1,48 @@
+let gamepad;
+let box = document.querySelector("#box");
 
-const log = (...any) => {
-    console.log(...any)
-}
-
-
-var gamepads = {};
-
-var gamepad;
-var gamepadPos;
-
-window.addEventListener(
-    "gamepadconnected",
-    function (e) {
-        gamepads = navigator.getGamepads();
-        gamepadPos = e.gamepad.index;
-        gamepad = navigator.getGamepads()[e.gamepad.index];
-    },
-    false
+window.addEventListener("gamepadconnected",
+    () => {
+        gamepad = navigator.getGamepads()[0];
+        console.log("New controller! ", gamepad)
+    }
 );
 
 /*
 window.addEventListener(
     "gamepaddisconnected",
     function (e) {
-        gamepadHandler(e, false);
+
     },
     false
 );*/
 
+const update = () => {
+    gamepad = navigator.getGamepads()[0];
 
-
-function updateControllers() {
-
-
-    log(gamepad.buttons[0].pressed)
-
-    gamepads = navigator.getGamepads();
-    gamepad = navigator.getGamepads()[gamepadPos];
-
-    log(gamepads)
-    
-    log(gamepad.buttons[0].pressed)
-
-}
-
-
-function playVibration() {
-
-    gamepad.vibrationActuator.playEffect("dual-rumble", {
-        startDelay: 0,
-        duration: 2000,
-        weakMagnitude: .5,
-        strongMagnitude: .5
-    });
-
-}
-
-
-
-setInterval(function() {
-    updateControllers()
-    if (gamepad.buttons[0].pressed) {
-        log("Button pressed. Viiibbbbrrraaaatttinnnggggg.......")
-        playVibration();
+    if (gamepad !== null) {
+        if (gamepad.buttons[0].pressed) {
+            console.log("Button pressed. Changing color!")
+            box.style.backgroundColor = "#ff0000";
+        } else if (gamepad.buttons[5].pressed) {
+            setTimeout(() => {
+                console.log(gamepad);
+            }, 1000);
+        } else {
+            box.style.backgroundColor = "#ffffff";
+        }
     }
 
-}, 1);
+    window.requestAnimationFrame(update);
+};
 
-    
+window.requestAnimationFrame(update)
 
+/*
+gamepad.vibrationActuator.playEffect("dual-rumble", {
+    startDelay: 0,
+    duration: 2000,
+    weakMagnitude: .5,
+    strongMagnitude: .5
+});
+*/
